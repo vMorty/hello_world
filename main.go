c2f62e2c3de8e36498b1be77222d6285863eb5dd
vMorty <xxrainingfire@aol.com>
2024-02-12 20:57:55 -0500

    Your commit message here

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func add(x, y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Println("hello world")
+	num1 := 15
+	num2 := 18
+	result := add(num1, num2)
+	fmt.Printf("%d +%d = %d\n", num1, num2, result)
 }
